ae70e1d63e252e65613be450aa2bfc4562c6bfef
Arekkasu <Alexanderlozada230@gmail.com>
2022-04-17 20:13:48 -0500

    Mas talleres agregados

---
+++ Proyectos/Edad Adulto.py
@@ -0,0 +1,9 @@
+# Detector si es adulto o menor de edad
+yearAdult = 18
+
+year = int(input('Ingresa tu edad: '))
+
+if year >= yearAdult:
+    print('Eres Mayor de edad')
+else:
+    print('Eres menor de edad')
---
+++ Proyectos/Numero par o impar.py
@@ -0,0 +1,7 @@
+
+# Saber si su numero es par o impar
+number = int(input("Ingresa el numero: "))
+if number % 2 == 0:
+    print(f'El valor {number} es par')
+else:
+    print(f'El valor {number} no es par')
