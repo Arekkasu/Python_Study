e908442999efe2a190c56f1a45fb77bdb74426b1
Arekkasu <Alexanderlozada230@gmail.com>
2022-08-04 22:34:22 -0500

    Actualizacion de Lista

--- Seccion 7/Listas/Listas.py
+++ Seccion 7/Listas/Listas.py
@@ -1,6 +1,9 @@
 '''Las listas son mutables (SE PUEDEN MODIFICAR, LAS LISTAS TIENEN UN INICIO Y UN FINAL
 HACIENDO QUE LAS LISTAS SEAN SECUENCIALES'''
 
+from tokenize import Number
+
+
 lista = list(range(0,20,2))
 print(lista)
 
@@ -28,7 +31,7 @@ print(lista[5:0:-1])
 Matriz = [[0,1,2],	#lista con ID[0],
 		  [3,4,5],
 		  [6,7,8],	#LISTAS CON ID[2]
-				 ]
+				]
 print(Matriz[2][1])
 
 #Operar con listas
@@ -73,3 +76,79 @@ print(li)
 
 l2 = [x**2 for x in num if x < 5]
 print(l2)
+
+#Referencias y apuntadores
+"""En esto se refiere a como las variables tienen su refetencia o como son apuntadas en la
+memoria del Dispositivo"""
+
+a = 'hola'
+b = 'hola'
+
+#aqui dara True debido a que los STR son inmutables y tiene su propia direccion de memoria
+
+#Se refiere si el objeto de las variables son el mismo de la memoria
+print(a is b)
+
+#Referencias y apuntadores en listas
+
+print("""El usar 'IS' no dara True, debido a que la lista si pueden obtener igual de elemento pero son distintos en la
+ubicacion de la memoria""")
+
+A = [1, 2, 3, 4]
+B = [1, 2, 3, 4]
+
+#Dara False
+print(A is B)
+
+#Dara True
+print(A == B)
+
+#como Hacer que compartan el mismo Objeto
+
+#Ahora haremos esto
+
+C = A
+
+print(A is C)
+
+print('Vamos a mirar el ID 1 de las listas C y A')
+print()
+print(A[1])
+print(C[1])
+print(A[1] == C[1])
+
+#Si hago una modificacion en C o en A, afectara a toda aqueva variable que sea el mismo objeto de este
+
+C[1] = 'Se modifico'
+
+print(f'A[1]: {A[1]}')
+print(f'C[1]: {C[1]}')
+
+#Hacer una copia de una lista sin ser afectada la original
+
+#copy()
+
+D = A.copy()
+
+print(f'metodo D.copy() {D}')
+
+#ejemplo mas elaborado
+
+Numbers = [1, 2, 3]
+Copy_Numbers = Numbers[:] # --> copia de Numbers
+
+Numbers[0] = 'Se'
+Numbers[1] = 'Cambio'
+
+print(Numbers)
+
+print(Copy_Numbers)
+
+
+print('La modificacion')
+print()
+print(Numbers is Copy_Numbers) # --> Falso debido a que es una compia y tiene su propia ubicacion en la memoria
+print()
+print(f'como queda la lista Numbers --> {Numbers}')
+print()
+print(f'Como queda la copia --> {Copy_Numbers}')
